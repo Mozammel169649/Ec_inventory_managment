93a77dcf499e57d9e31eb611a5d2f201d2ce0e38
Mozammel169649 <mozammel169649@gmail.com>
2023-11-26 12:01:08 +0600

    main route setup

--- app/admin/index.jsx
+++ app/admin/index.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider, createHashRouter,} from 'react-router-dom';
-
-import Dashboard from './Dashboard';
+import Layouts from './layouts/Layouts';
+import Dashboard from './pages/Dashboard';
+import user_router from './pages/management/user/config/router';
+import product_router from './pages/management/product/config/router';
 
 function Component() {
     const router = createHashRouter([
         {
             path: "/",
-            element: <Dashboard></Dashboard>
+            element : <Layouts></Layouts>,
+            children : [
+                {
+                    path : "",
+                    element : <Dashboard></Dashboard>
+                },
+                user_router,
+                product_router
+
+            ]
         }
     ])
 
