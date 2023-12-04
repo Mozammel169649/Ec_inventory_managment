2916a6ea17acc0a0fa755f6eed40422a0ee822af
Mozammel169649 <mozammel169649@gmail.com>
2023-12-04 12:57:19 +0600

    mongoose and db config

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const server = express()
 const port = 4001
-
+const mongoose = require('mongoose')
+const { db_url } = require('./configs/db_config')
 
 server.set('view engine', 'ejs');
 server.set('views', './views')
@@ -12,6 +13,19 @@ server.get('/', (req, res) => {
   res.send('Ec_inventory_management..!')
 })
 
-server.listen(port, () => {
-  console.log(`127.0.0.1:${port}`)
-})
+
+
+// mongoose.connect("mongodb+srv://mozammel:XDV3cNkHABTYeoFS@cluster0.yp55jwf.mongodb.net/blogDB")
+// .then(()=>{
+//   console.log("db_conneced")
+// })
+
+
+mongoose.connect(db_url)
+  .then(() => {
+    console.log("db_conneced")
+        server.listen(port, () => {
+          console.log(`127.0.0.1:${port}`)
+        })
+  })
+
