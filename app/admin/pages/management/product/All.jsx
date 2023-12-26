51287a5ce507000a4aa260f0fd37d23b490857a3
Mozammel169649 <mozammel169649@gmail.com>
2023-12-27 01:48:18 +0600

    data table demo

--- app/admin/pages/management/product/All.jsx
+++ app/admin/pages/management/product/All.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
 
 function All() {
+  
+  
+
   return (
     <div>
-          All products
+      <h5> All Products</h5>
+      <table className="table table-success table-striped">
+        <thead>
+          <tr>
+            <td>Id</td>
+            <td>Image</td>
+            <td>Number</td>
+            <td>Title</td>
+            <td>Satus</td>
+            <td>Stoke</td>
+            <td>Brand</td>
+            <td>Creator</td>
+            <td>Supplayer</td>
+            <td>Product_issu</td>
+            <td>Price</td>
+            <td>Discount</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+             
+          </tr>
+        </tbody>
+      </table>
     </div>
   )
 }
