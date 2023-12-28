99c0f5077e05e3741a68807a55c551a06d2bc9a0
Mozammel169649 <mozammel169649@gmail.com>
2023-12-28 18:18:58 +0600

    user list

--- app/admin/pages/management/user/All.jsx
+++ app/admin/pages/management/user/All.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 
 function All() {
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    const modelData = async () => {
+      const response = await axios.get('/api/v1/user')
+      setUser(response.data)
+      console.log(response.data)
+    }
+    modelData();
+  }, []);
+
   return (
     <div>
-        all User
+      <h5> All User</h5>
+      <table className="table table-success table-striped">
+        <thead>
+          <tr>
+            <td>Id</td>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Role</td>
+            <td>contuct</td>
+            <td>Address</td>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            user.map(ele =>
+              <tr>
+                <td>{ele?._id}</td>
+                <td>{ele?.userName}</td>
+                <td>{ele?.email}</td>
+                <td>{ele?.role}</td>
+                <td>{ele?.contuct}</td>
+                <td>{ele?.address}</td>
+              </tr>
+
+            )
+          }
+        </tbody>
+      </table>
     </div>
   )
 }
