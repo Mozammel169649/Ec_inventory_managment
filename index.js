b0a642dc5d3cb997cffffc4a2b57a7405aa85aa3
Mozammel169649 <mozammel169649@gmail.com>
2024-01-02 23:46:00 +0600

    change port, set isAuth, isAdmin, user

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const server = express()
-const port = 4001
+const port = 5555
 const mongoose = require('mongoose')
 const { db_url } = require('./configs/db_config')
 const allRoute = require('./routes/all.route.js')
@@ -8,6 +8,9 @@ const formData = require('express-form-data');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const session = require('express-session');
+const isAuthMiddelware = require('./app/middelware/isAuth.middelware.js')
+const { render } = require('ejs')
+const isAdminMiddelware = require('./app/middelware/isAdmin.middelware.js')
 
 server.use(express.static("public"));
 server.use(bodyParser.json());
@@ -19,51 +22,73 @@ server.use(cookieParser());
 server.set('json spaces', 4);
 server.set('trust proxy', 1);
 server.use(session({
-    secret: 's3Cur3',
-    name: 'sessionId'
+  secret: 's3Cur3',
+  name: 'sessionId'
 }))
 server.set('view engine', 'ejs');
 server.set('views', './views')
 server.use(express.static("public"))
 
 
-server.get('/', (req, res) => {
-  res.render('home')
-})
+
+// server.use((req, res, next) => {
+//   console.log("it's ok")
+//   isAuthMiddelware(server,req,res,next);
+//   isAdminMiddelware(server,req,res,next);
+//   next();
+// })
+
 
 server.use(async (req, res, next) => {
+
   server.locals.error = {};
   server.locals.old = {};
   server.locals.login_error = {};
   server.locals.login_old = {};
- 
+
+  server.locals.isAuth = false;
+  server.locals.isAdmin = false;
+  server.locals.user = {};
+
   if (req.session.login_old) {
-      server.locals.login_old = req.session.login_old;
-      req.session.login_old = {}
-      console.log()
+    server.locals.login_old = req.session.login_old;
+    req.session.login_old = {}
+    console.log()
   }
   if (req.session.login_error) {
-      server.locals.login_error = req.session.login_error;
-      req.session.login_error = {}
+    server.locals.login_error = req.session.login_error;
+    req.session.login_error = {}
   }
   if (req.session.error) {
-      server.locals.error = req.session.error;
-      req.session.error = {};
+    server.locals.error = req.session.error;
+    req.session.error = {};
   }
   if (req.session.old) {
-      server.locals.old = req.session.old;
-      req.session.old = {};
+    server.locals.old = req.session.old;
+    req.session.old = {};
   }
-  console.log(req.session);
+  if (req.session.isAuth) {
+    server.locals.isAuth = req.session.isAuth;
+    req.session.isAuth = false;
+  }
+  if (req.session.isAdmin) {
+    server.locals.isAdmin = req.session.isAdmin;
+    req.session.isAdmin = false;
+  }
+  if (req.session.user) {
+    server.locals.user = req.session.user;
+    req.session.user = {};
+  }
+  console.log(server.locals)
   next();
 })
 
-// server.get('/dashboard/user', (req, res) => {
-//   res.send("right roting")
-// })
-
 server.use(allRoute());
 
+server.use((req,res , next)=>{
+  console.log("helo2")
+  next();
+})
 
 // mongoose.connect("mongodb+srv://mozammel:XDV3cNkHABTYeoFS@cluster0.yp55jwf.mongodb.net/blogDB")
 // .then(()=>{
@@ -74,8 +99,8 @@ server.use(allRoute());
 mongoose.connect(db_url)
   .then(() => {
     console.log("db_conneced")
-        server.listen(port, () => {
-          console.log(`app is listening on http://127.0.0.1:${port}`);
-        })
+    server.listen(port, () => {
+      console.log(`app is listening on http://127.0.0.1:${port}`);
+    })
   })
 
