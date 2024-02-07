d36a4332797a3b2e0a85888919ae6a2550eb9a87
mozammel <mozammel169649@gmail.com>
2024-02-07 16:27:40 +0600

    auth routes

--- routes/partials/auth/auth.routes.js
+++ routes/partials/auth/auth.routes.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const { authController } = require("../../../app/controller/auth/auth.controller");
+const authController = require("../../../app/controller/auth/auth.controller");
 const router = express.Router()
 
 router
-    // .post("/signup-submit", authController.signUp)
-    // .post("/login-submit", authController.login)
+    .post("/signUp_submit", authController.signUp)
+    .post("/login-submit", authController.login)
     // .get("/logout", authController.logout)
 
 module.exports = () => router;
