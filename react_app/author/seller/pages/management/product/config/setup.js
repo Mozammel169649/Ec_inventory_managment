6fc2b06de725e0573ba084ef87861ec10a499191
mozammel <mozammel169649@gmail.com>
2024-02-07 16:09:50 +0600

    delete files

--- react_app/author/seller/pages/management/product/config/setup.js
+++
@@ -1,5 +0,0 @@
-const setup = {
-    route_prefix: "product",
-    page_title: "Product Management"
-}
-export default setup ;
--- react_app/author/seller/pages/management/user/config/setup.js
+++
@@ -1,5 +0,0 @@
-const setup = {
-    route_prefix :"user",
-    page_title : "User Management"
-}
-export default setup ;
