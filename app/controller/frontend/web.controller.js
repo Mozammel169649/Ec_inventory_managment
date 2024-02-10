d2d1be3b2151a7cc259d90a71b765384bceb5bb4
mozammel <mozammel169649@gmail.com>
2024-02-10 23:09:51 +0600

    web controller

--- app/controller/frontend/web.controller.js
+++ app/controller/frontend/web.controller.js
@@ -5,8 +5,10 @@ const controller ={
     navDinamic: async function(req,res){
        const response ={};
         const token = req.cookies?.atoken;
+        console.log("token :" ,token);
          if(token){
-            let user = await jwt.verify("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyTmFtZSI6InNhZ2lyIiwiZW1haWwiOiJzYWdpckBnbWFpbC5jb20iLCJfaWQiOiI2NWMzNGQ3MTFmMTE0ZjJmNWIyNjE2YjUiLCJyb2xlIjoic2VsbGVyIiwicGhvdG9fdXJsIjoiIiwiZGV2aWNlX2lkIjoiIiwiZ2VucmF0ZV90aW1lIjoiIiwiaWF0IjoxNzA3MzMyNDEwfQ.SCxg1uRusomRsJFhYPwu51m_H08cQkpWG8J__gDQpJY", '6fd286f7-708a-429b-b53a-2bc5272e0db6');
+            let user = await jwt.verify(token, '6fd286f7-708a-429b-b53a-2bc5272e0db6');
+            console.log(user);
             response.user = user ;
             return res.json(response);
          }else{
