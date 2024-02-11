47c70d2bd9efbbcaaacbf49ff941e783d3b585f0
mozammel <mozammel169649@gmail.com>
2024-02-11 22:03:41 +0600

    user view

---
+++ react_app/author/admin/pages/management/user/pages/View.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function View() {
+  return (
+    <div className='col-md-6 mx-auto'>
+      <center>
+        <h3> User Detalse </h3>
+      </center>
+      <div className='text-center mt-4 '>
+        <p>UserName : mozammel</p>
+        <p>UserEmail : mozammel</p>
+        <p>UserRole : Admin</p>
+      </div>
+
+    </div>
+  )
+}
+
+export default View
