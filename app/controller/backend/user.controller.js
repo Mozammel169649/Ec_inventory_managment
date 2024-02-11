54a22a0c28d3c97e08583f11c483ad6422d1f4a6
mozammel <mozammel169649@gmail.com>
2024-02-11 22:02:48 +0600

    crud operation with redux

---
+++ app/controller/backend/user.controller.js
@@ -0,0 +1,23 @@
+const userModel = require("../../models/product_models/user.model");
+
+const controller = {
+    all: async function (req, res) {
+        const data = await userModel.find();
+        return res.json(data);
+    },
+    update: async function (req, res){
+        const id = req.params.id;
+        const {userName,email,role} = req.body?.updateUser;
+        console.log("id email", email, id);
+        const user = await userModel.findByIdAndUpdate({_id:id},{userName:userName, email:email , role:role});
+        console.log("user from update controller",user);
+        return res.json(user);
+    },
+    delete: async function(req,res){
+        const id = req.params.id;
+        const user = await userModel.findByIdAndDelete({_id:id});
+        return res.json(user);
+    }
+}
+
+module.exports = controller;
