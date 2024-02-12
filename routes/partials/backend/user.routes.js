fa4388dcf1e0d81356381444e8f206dbc0d281b8
mozammel <mozammel169649@gmail.com>
2024-02-12 18:23:33 +0600

    connect userController

--- routes/partials/backend/user.routes.js
+++ routes/partials/backend/user.routes.js
@@ -1,12 +1,9 @@
 const express = require('express');
-const userModel = require('../../../app/models/product_models/user.model');
-const checkAuthMiddelware = require('../../../app/middelware/isAuth.middelware');
+const userController = require('../../../app/controller/backend/user.controller');
 const router = express.Router()
 
 router
-    .get("/user", async function (req, res) {
-        const data = await userModel.find();
-        return res.json(data);
-    });
-
+    .get("/user", userController.all )
+    .post("/updateUser/:id", userController.update )
+    .get("/delete/:id", userController.delete);
 module.exports = () => router;
