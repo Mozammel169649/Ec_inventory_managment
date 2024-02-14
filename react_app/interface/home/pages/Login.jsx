473bdfba3d2462733ade7a47753a1c41b47bd0c0
mozammel <mozammel169649@gmail.com>
2024-02-14 10:05:58 +0600

    login

--- react_app/interface/home/pages/Login.jsx
+++ react_app/interface/home/pages/Login.jsx
@@ -1,47 +1,75 @@
+import axios from 'axios';
 import React from 'react'
-import { useState } from 'react'
+import { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+
 
 function Login() {
-  const [loginData, setloginData] = useState({ email:"", password:"" });
+  const [responce, setResponce] = useState({});
+  const navigate = useNavigate();
 
-  // const onchangeUser =(e)=>{
-  //   setloginData({...loginData, email: e.target.value })
-  //   console.log(loginData)
-  // }
+  const loginSubmit = async (event) => {
+    event.preventDefault();
+    axios.post("/login-submit", new FormData(event.currentTarget))
+      .then(res => {
+        setResponce(res.data);
 
+        if (res.data?.success) {
+          const { userRole } = res.data;
+          if (userRole === "user") {
+          //  return location.href = "/";
+            return navigate("/");
+          }
+          if (userRole === "admin") {
+            return location.href = "http://127.0.0.1:3000/dashboard/admin";
+          }
+          if (userRole === "manager") {
+            return location.href = "http://127.0.0.1:3000/dashboard/manager";
+          }
+          if (userRole === "seller") {
+            return location.href = "http://127.0.0.1:3000/dashboard/seller";
+          }
+        }
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
 
   return (
-    <div>
-       <section className="mb-4">
-         <div className="container col-6">
-            <center className="pt-2">
-                <h1>Login</h1>
-            </center>
-            <form method="POST" className=" mx-auto" action="/login-submit">
-                <div className="form-group mb-4">
+    <div className="">
+      <section >
+        <div className="col-6 mx-auto">
+          <center className="pt-2">
+            <h1>Login</h1>
+            <p style={{ "color": "green" }}>{responce?.success}</p>
+          </center>
+          <div cassName=''>
+            <form method='POST' onSubmit={loginSubmit} className=" mx-auto" autocomplete="false">
+              <div className="form-group mb-4">
                 <label for="email">Email</label>
-                <input className="form-control" onChange={(e)=>setloginData({ ...loginData, user:e.target.value })} type="email" name="email" id="email"/>
-                {/* <input className="form-control" onChange={onchangeUser} type="email" name="email" id="email"/> */}
+                <input className="form-control" type="email" name="email" id="email" />
                 <div className="text-danger">
-                
+                  <p style={{ "color": "red" }}>{responce?.email}</p>
                 </div>
-                </div>
-                <div className="form-group mb-4">
+              </div>
+              <div className="form-group mb-4">
                 <label for="pass">Password</label>
-                <input className="form-control" onChange={(e)=>setloginData({ ...loginData, password:e.target.value })} type="password" name="password" id="pass"/>
+                <input className="form-control" type="password" name="password" id="password" />
                 <div className="text-danger">
-                
+                  <p style={{ "color": "red" }}>{responce?.password}</p>
                 </div>
-                </div>
-                <button type="submit" className="btn btn-success">Login</button>
+              </div>
+              <button type="submit" className="btn btn-success">Login</button>
             </form>
-            <center>
-                <div className="d-grid gap-2 col-6 mx-auto">
-                <a href="/#register" className="btn btn-primary">Sign_UP</a>
-                </div>
-            </center>
-         </div>
-       </section>    
+          </div>
+          <center>
+            <div className="d-grid gap-2 col-6 mx-auto">
+              <a href="/#register" className="btn btn-primary">Sign_UP</a>
+            </div>
+          </center>
+        </div>
+      </section>
     </div>
   )
 }
