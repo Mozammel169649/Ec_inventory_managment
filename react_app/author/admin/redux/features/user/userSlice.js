39c85242d9938c59183f6c908e8e5b28b2ef98e8
mozammel <mozammel169649@gmail.com>
2024-02-18 23:18:03 +0600

    userSlice js

--- react_app/author/admin/redux/features/user/userSlice.js
+++ react_app/author/admin/redux/features/user/userSlice.js
@@ -1,29 +1,46 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import axios from 'axios'
+
+
+export const fatchData = createAsyncThunk("users/fatchData", async()=>{
+  console.log('ghghg');
+  const res = await axios.get("/user");
+  return res.data;
+})
+
+
 
 const userSlice = createSlice({
   name: 'users',
   initialState: {
     users: []
   },
-  reducers: {
-
-    getUser: (state, action) => {
-      state.users = action.payload.map(user => {
-        return { id: user._id, userName: user.userName, email: user.email, role: user.role }
-      })
-    },
-    updateUserData: (state, action) => {
-      const index = state.users.findIndex(e => e._id === action.payload.id)
-      state.users[index] = {
-        email: action.payload.email,
-        userName: action.payload.userName,
-        role: action.payload.role,
-      }
-    },
-    deleteUser:(state, action)=>{
-      state.users =state.users.filter( (e) => e.id !== action.payload );
-     }
-  },
+  // reducers: {
+  //   getUser: async (state) => {
+  //     console.log("getUser ok")
+  //     const response = await axios.get('/user')
+  //     // console.log("api ok", response)
+  //     state.users = response.data;
+  //     // console.log("state",state.users);
+  //   },
+  //   updateUserData: (state, action) => {
+  //     const index = state.users.findIndex(e => e._id === action.payload.id)
+  //     state.users[index] = {
+  //       email: action.payload.email,
+  //       userName: action.payload.userName,
+  //       role: action.payload.role,
+  //     }
+  //   },
+  //   deleteUser:(state, action)=>{
+  //     state.users =state.users.filter( (e) => e.id !== action.payload );
+  //    }
+  // },
+  extraReducers : (builder)=>{
+    builder.addCase(fatchData.fulfilled, (state, action) =>{
+      state.users = action.payload;
+      console.log(state);
+    })
+  }
 })
 
 export const { getUser, updateUserData , deleteUser} = userSlice.actions;
