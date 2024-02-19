515e1ed4dc7ae8c47294974099464be8dd6d5142
mozammel <mozammel169649@gmail.com>
2024-02-19 21:52:31 +0600

    update user controller

--- app/controller/backend/user.controller.js
+++ app/controller/backend/user.controller.js
@@ -7,16 +7,18 @@ const controller = {
         return res.json(data);
     },
     update: async function (req, res){
-        const id = req.params.id;
-        const {userName,email,role} = req.body?.updateUser;
-        console.log("id email", email, id);
+        const id = req.body.id;
+        const {userName,email,role} = req.body;
+        // console.log(req.body);
         const user = await userModel.findByIdAndUpdate({_id:id},{userName:userName, email:email , role:role});
-        console.log("user from update controller",user);
+        // console.log(user);
         return res.json(user);
     },
     delete: async function(req,res){
+        console.log("hello");
         const id = req.params.id;
         const user = await userModel.findByIdAndDelete({_id:id});
+        // console.log(user);
         return res.json(user);
     }
 }
