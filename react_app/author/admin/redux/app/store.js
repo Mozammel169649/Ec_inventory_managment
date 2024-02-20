5d3fa4fe0614a5aba19861e1da887b9874b63e72
mozammel <mozammel169649@gmail.com>
2024-02-20 17:55:45 +0600

    store

--- react_app/author/admin/redux/app/store.js
+++ react_app/author/admin/redux/app/store.js
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "../features/user/userSlice";
+import dataSlice from "../features/user/dataSlice";
+import roleSlice from "../features/user/roleSlice";
 
 const store = configureStore({
     reducer: {
-      users : userSlice
+      users : userSlice,
+      data : dataSlice,
+      role : roleSlice
     }
 })
 
