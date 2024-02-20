1253163e93ca1eda72760c9e495a8ae9450aabe9
mozammel <mozammel169649@gmail.com>
2024-02-20 17:55:14 +0600

    role controller

--- app/controller/backend/role.controller.js
+++ app/controller/backend/role.controller.js
@@ -2,7 +2,7 @@ const userRoleModel = require("../../models/user_models/userRole.model");
 
 const controller = {
     all: async function (req, res) {
-        console.log("i am from user role api")
+        // console.log("i am from user role api")
         const data = await userRoleModel.find();
         return res.json(data);
     },
