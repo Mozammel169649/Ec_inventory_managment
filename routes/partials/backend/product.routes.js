ca3ffbb3b59b6aaba85c86c9337b439f683ef569
mozammel <mozammel169649@gmail.com>
2024-02-22 18:23:56 +0600

    product route

--- routes/partials/backend/product.routes.js
+++ routes/partials/backend/product.routes.js
@@ -4,9 +4,30 @@ const checkAuthMiddelware = require("../../../app/middelware/isAuth.middelware")
 const router = express.Router()
 
 router
-    .get("/product",async function (req,res) {
+    .get("/product", async function (req, res) {
         const data = await productModel.find();
         return res.json(data);
-    });
+    })
 
-module.exports = ()=> router;
+
+    
+
+    .get("/t", async function (req, res) {
+        return res.json([{ a: 1 }, { a: 2 },{ a: 3 }]);
+    })
+    .get("/t/:id", async function (req, res) {
+        return res.json({ a: req.params.id });
+    })
+    .post("/t", async function (req, res) {
+        return res.json(req.body);
+    })
+    .put("/t/:id", async function (req, res) {
+        return res.json(req.body);
+    })
+    .delete("/t/:id", async function (req, res) {
+
+        return res.json("delete "+req.params.id);
+    })
+    ;
+
+module.exports = () => router;
