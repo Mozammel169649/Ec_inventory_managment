b681cfde8123e9434d026bb12802fc76f572caec
mozammel <mozammel169649@gmail.com>
2024-03-02 19:40:44 +0600

    supplier

---
+++ app/controller/backend/supplier.controller.js
@@ -0,0 +1,43 @@
+const jwt = require('jsonwebtoken');
+const supplierModel = require('../../models/product_models/supplier.model');
+
+const controller = {
+    all: async (req, res) => {
+        // console.log("all route ok");
+        const data = await supplierModel.find().populate('creator');
+        // console.log("from supplier model",data);
+        return res.json(data);
+    },
+    create: async (req, res) => {
+        const { atoken } = req.cookies
+        const decoded = jwt.decode(atoken, '6fd286f7-708a-429b-b53a-2bc5272e0db6', algorithms = ['HS256'])
+        const {name, email, number, address, discription} = req.body
+        console.log(req.body);
+        const Data = new supplierModel({
+            name: name,
+            address:address,
+            email:email,
+            number:number,
+            discription:discription,
+            creator : decoded._id
+        });
+        await Data.save();
+        return res.json(Data);
+    },
+    update: async (req, res) => {
+        const { id, name, email, number, address, discription } = req.body;
+        // console.log(req.body);
+        const user = await supplierModel.findByIdAndUpdate({ _id: id }, { name: name, email: email, number: number, address: address, discription: discription });
+        // console.log("update user", user);
+        return res.json(user);
+    },
+    delete: async (req, res) => {
+        // console.log("hello");
+        const id = req.params.id;
+        const user = await supplierModel.findByIdAndDelete({ _id: id });
+        // console.log(user);
+        return res.json(user);
+    }
+
+}
+module.exports = controller;
