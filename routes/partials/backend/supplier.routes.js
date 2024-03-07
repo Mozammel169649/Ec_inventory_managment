37abd4751733ee82139c9de1a0fff52b161661f6
mozammel <mozammel169649@gmail.com>
2024-03-07 17:55:01 +0600

    supplier routes

---
+++ routes/partials/backend/supplier.routes.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const router = express.Router()
+const supplierController = require('../../../app/controller/backend/supplier.controller');
+router
+    .get("/supplier", supplierController.all)
+    .post("/createsupplier", supplierController.create)
+    .put("/updatesupplier/:id", supplierController.update)
+    .delete("/deletesupplier/:id", supplierController.delete)
+
+module.exports = () => router;
