3d5cf1019143ecda96a2e450d0f2bf134f432502
mozammel <mozammel169649@gmail.com>
2024-03-13 16:46:59 +0600

    brand layout

---
+++ react_app/author/admin/pages/management/brand/Layoute.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Outlet } from "react-router"
+import setup from "./config/setup"
+const {page_title} = setup;
+
+function Layoute() {
+    return (
+        <div>
+            <div className ="m-10" >
+                <h4 >{page_title}</h4>
+            </div>
+            <div>
+                <Outlet></Outlet>
+            </div>
+        </div>
+    )
+}
+
+export default Layoute;
