5c2906a33a2365448a241afab17fa992e9ff7b1c
mozammel <mozammel169649@gmail.com>
2024-03-14 17:13:30 +0600

    product stock seed

--- app/models/product_models/sedders/proudctStoke.sedder.js
+++ app/models/product_models/sedders/proudctStoke.sedder.js
@@ -1,15 +1,10 @@
 const { default: mongoose } = require("mongoose");
 const { db_url } = require("../../../../configs/db_config");
-const productModel = require("../product.model");
 const userModel = require("../user.model");
 const supplierModel = require("../supplier.model");
 const productStokeModel = require("../productStoke.model");
 
 
-const get_stoke_product_number = async ()=>{
-    let product = await productModel.findOne()
-    return product.product_number;
-} 
 const get_creater_id = async ()=> {
     let creator = await userModel.findOne()
     return creator._id;
@@ -18,15 +13,12 @@ const get_supplier_id = async ()=> {
     let supplier = await supplierModel.findOne()
     return supplier._id;
 }
-const get_productStok =async()=> {
-    let product = await productModel.findOne()
-    return product.stokes;
-} 
+
 
 const stoke = async()=>[
     {
-        product_number:await get_stoke_product_number(),
-        stoke: await get_productStok(),
+        product_code: "12r6je",
+        stock: 50 ,
         creator:await get_creater_id(),
         supplier:await get_supplier_id()
     }
