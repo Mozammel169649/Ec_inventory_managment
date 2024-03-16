d3f19a5f2cd440d0a285d7aa70c11a38b22fc3ad
mozammel <mozammel169649@gmail.com>
2024-03-16 17:05:26 +0600

    router

--- react_app/author/admin/pages/management/product/config/router.js
+++ react_app/author/admin/pages/management/product/config/router.js
@@ -1,5 +1,8 @@
 import All from "../All"
+import Create from "../Create"
+import Edit from "../Edit"
 import ProductLayoute from "../Layoute"
+import View from "../View"
 import setup from "./setup"
 
 const { route_prefix } = setup
@@ -12,5 +15,18 @@ export default {
             path: "",
             element: <All></All>
         },
+        {
+            path: "edit/:id",
+            element: <Edit></Edit>
+        },
+        {
+            path: "view/:id",
+            element: <View></View>
+        },
+        {
+            path: "create",
+            element: <Create></Create>
+        }
+       
     ]
 }
