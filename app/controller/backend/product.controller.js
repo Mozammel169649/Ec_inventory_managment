96fd114841e75df4f70ba088abf177b1a56d1133
mozammel <mozammel169649@gmail.com>
2024-03-24 17:47:12 +0600

    productcontroller

--- app/controller/backend/product.controller.js
+++ app/controller/backend/product.controller.js
@@ -1,55 +1,157 @@
 const jwt = require('jsonwebtoken');
 const productModel = require('../../models/product_models/product.model');
 
+var fs = require('fs-extra');
+const { dirname } = require('path');
+const appDir = dirname(require.main.filename);
+
+
+const upload_files = (file, id) => {
+    let file_name = parseInt(Math.random() * 1000) + id + file.name;
+
+    const path = appDir + "/public/uploads/posts/" + file_name;
+
+    fs.move(file.path, path, function (err) {
+        if (err) return console.error(err)
+        console.log("success!")
+    })
+    thumb_image_path = "uploads/posts/" + file_name;
+    return thumb_image_path;
+}
 
 const controller = {
-    all: async (req, res)=> {
-        console.log('res from controler',res.cookie);
-        const data = await productModel.find().populate([{ path:'categories'},{ path:'brand'},{ path:'creator'}]);
+    all: async (req, res) => {
+        console.log('res from controler', res.cookie);
+        const data = await productModel.find().populate([{ path: 'categories' }, { path: 'brand' }, { path: 'creator' }]);
         // console.log("from product model",data);
         return res.json(data)
     },
-    create : async(req, res)=>{
-        const {atoken} = req.cookies
-        const decoded = jwt.decode(atoken, '6fd286f7-708a-429b-b53a-2bc5272e0db6', algorithms=['HS256'])
-        console.log("product creator body data", req.body);
-        // const {} = req.body
+    singleData: async (req, res) => {
+        // const id = req.params.id;
+        console.log('res from controler', req.params);
+        const data = await productModel.findOne({ _id: req.params.id });
+        // console.log("from product model",data);
+        return res.json(data)
+    },
+    create: async (req, res) => {
+        const { atoken } = req.cookies
+        const decoded = jwt.decode(atoken, '6fd286f7-708a-429b-b53a-2bc5272e0db6', algorithms = ['HS256']);
+        var status = "";
+        if ("status" in req.body) {
+            status = true;
+            // console.log(status);
+        } else {
+            status = false;
+            // console.log(status);
+        }
+        // console.log("product creator body data", req.body);
 
-        // const Data = new brandModel({
-        //     // title:,
-        //     // product_number:,
-        //     // categories:,
-        //     // brand:,
-        //     // creator:,
-        //     // discount:,
-        //     // stokes:,
-        //     // varient:,
-        //     // supplier:,
-        //     // price:,
-        //     // short_discription:,
-        //     // discription:,
-        //     // seo_title:,
-        //     // status:,
-        //     // date:,
-        //     // image:,
-        //     // related_images: ,
-        //     // taqs:,
-        // });
-        // await Data.save();
-        // return res.json(Data);
+        const { title, code, seo_title, price, discount, supplier, stock, category, brand, varient, crt_price, short_discription, discription } = req.body;
+
+        let data = {
+            title: title,
+            product_number: code,
+            categories: category,
+            brand: brand,
+            creator: decoded,
+            discount: discount,
+            stokes: stock,
+            varient: varient,
+            supplier: supplier,
+            price: price,
+            short_discription: short_discription,
+            discription: discription,
+            seo_title: seo_title,
+            status: status,
+            current_price: crt_price,
+        };
+
+        let product = {};
+        try {
+            product = await productModel.create(data);
+
+            var image_path = "";
+            var related_image = [];
+
+            // console.log(req.files);
+
+            if (req.files?.image && req.files?.image.size) {
+                image_path = upload_files(req.files.image, product._id)
+            }
+            if (req.files?.rtd_image && req.files?.rtd_image[0].size) {
+                related_image = req.files.rtd_image.map((file) => upload_files(file, product._id));
+            }
+
+            product.image = image_path;
+            product.related_images = related_image;
+            product.save();
+
+        } catch (error) {
+
+            return res.status(500).json({ msg: "data uploading failed.", error: error })
+        }
+        return res.json(product);
     },
-    update: async (req, res)=>{
+    update: async (req, res) => {
         const id = req.body.id;
-        const {title} = req.body;
-        // console.log(req.body);
-        const user = await brandModel.findByIdAndUpdate({_id:id},{title : title});
-        // console.log(user);
-        return res.json(user);
+        var status = "";
+        if ("status" in req.body) {
+            status = true;
+            // console.log(status);
+        } else {
+            status = false;
+            // console.log(status);
+        }
+        const { title, code, seo_title, price, discount, supplier, stock, category, brand, varient, crt_price, short_discription, discription } = req.body;
+       
+        let data = {
+            title: title,
+            product_number: code,
+            categories: category,
+            brand: brand,
+            creator: decoded,
+            discount: discount,
+            stokes: stock,
+            varient: varient,
+            supplier: supplier,
+            price: price,
+            short_discription: short_discription,
+            discription: discription,
+            seo_title: seo_title,
+            status: status,
+            current_price: crt_price,
+        };
+
+        let product = {};
+        try {
+            product = await blogModel.findOneAndUpdate({ _id: req.params.id }, data);
+        
+            var image_path = product.image || "";
+            var related_image = product.related_images || [];
+            // console.log(req.files);
+
+            if (req.files?.image && req.files?.image.size) {
+                image_path = upload_files(req.files.image, product._id)
+            }
+            if (req.files?.rtd_image && req.files?.rtd_image[0].size) {
+                related_image = req.files.rtd_image.map((file) => upload_files(file, product._id));
+            }
+
+            product.image = image_path;
+            product.related_images = related_image;
+            product.save();
+
+        } catch (error) {
+
+            return res.status(500).json({ msg: "data uploading failed.", error: error })
+        }
+        return res.json(product);
+
     },
-    delete: async (req, res)=>{
+    delete: async (req, res) => {
         // console.log("hello");
         const id = req.params.id;
-        const user = await brandModel.findByIdAndDelete({_id:id});
+        const user = await productModel.findByIdAndDelete({ _id: id });
         // console.log(user);
         return res.json(user);
     }
