e628246de3d61347cd1bcd972de46830e96dab4c
mozammel <mozammel169649@gmail.com>
2024-03-24 17:46:13 +0600

    add single supplier route

--- routes/partials/backend/supplier.routes.js
+++ routes/partials/backend/supplier.routes.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const supplierController = require('../../../app/controller/backend/supplier.controller');
 router
     .get("/supplier", supplierController.all)
+    .get("/supplier/:id", supplierController.singleData)
     .post("/createsupplier", supplierController.create)
     .put("/updatesupplier/:id", supplierController.update)
     .delete("/deletesupplier/:id", supplierController.delete)
