c5e10ad25bfa5ca41a2b36b0a56acb61a88f9175
mozammel <mozammel169649@gmail.com>
2024-03-27 16:52:28 +0600

    web controller

--- app/controller/frontend/web.controller.js
+++ app/controller/frontend/web.controller.js
@@ -5,7 +5,7 @@ const controller ={
     navDinamic: async function(req,res){
        const response ={};
         const token = req.cookies?.atoken;
-        console.log("token :" ,token);
+      //   console.log("token :" ,token);
          if(token){
             let user = await jwt.verify(token, '6fd286f7-708a-429b-b53a-2bc5272e0db6');
             console.log(user);
