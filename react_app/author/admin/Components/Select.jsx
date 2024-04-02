270390bb800169ad3e80b58f56e6b667c7ad425d
mozammel <mozammel169649@gmail.com>
2024-04-02 20:23:52 +0600

    selector

---
+++ react_app/author/admin/Components/Select.jsx
@@ -0,0 +1,58 @@
+import React, { useRef, useState } from 'react'
+
+function Select({data, supplier, setSupplier}) {
+    const [temp, setTemp] = useState(() => [...data]);
+    // let seleced_list_html = useRef();
+
+    // function set(item) {
+        
+    //     let array = [...selected, item];
+    //     const uniqueArray = Array.from(new Set(array.map(obj => JSON.stringify(obj))))
+    //         .map(str => JSON.parse(str));
+    //     setSelected(uniqueArray);
+    // }
+
+    // function search_item(e) {
+    //     let value = e.target.value;
+    //     setTemp(data.filter(i => i.title.includes(value)));
+    //     if (value.length == 0) {
+    //         setTemp([...data]);
+    //     }
+    // }
+
+    // function remove_item(id) {
+    //     setSelected(selected.filter(i => i.id != id));
+    // }
+
+    return (
+        <div>
+            <div>
+                <ol
+                    style={{ minHeight: '40px' }}
+                    className='d-flex gap-4 border border-info rounded'>
+                    {
+                        // selected.map((i) => <li key={i.id}>
+                        //     <i onClick={() => remove_item(i.id)} class="bx bx-trash"></i>
+                        //     {i.title}
+                        // </li>)
+                    }
+                </ol>
+            </div>
+
+            <div style={{ padding: '5px', border: '1px solid red' }}>
+                <div>
+                    <input type="text" />
+                </div>
+                <ul>
+                    {
+                        temp.map((i) => <li onClick={() => set(i)} key={i.id}>
+                            {i.title}
+                        </li>)
+                    }
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Select
