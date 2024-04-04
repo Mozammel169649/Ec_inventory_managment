239464dc27dc474f9c06ce57ae16e09a978520e1
mozammel <mozammel169649@gmail.com>
2024-04-04 17:50:54 +0600

    website route

--- routes/partials/frontend/website.routes.js
+++ routes/partials/frontend/website.routes.js
@@ -6,18 +6,6 @@ router
     .get("/", function (req, res) {
       return res.render("frontend/pages/home");
     })
-    .get("/dashboard/admin", function (req, res) {
-        return res.render("backend/pages/admin/dashboard");
-    })
-    .get("/dashboard/seller", function (req, res) {
-        return res.render("backend/pages/seller/dashboard");
-    })
-    .get("/dashboard/manager", function (req, res) {
-        return res.render("backend/pages/manager/dashboard");
-    })
-    
-
     .get("/homeNav", webController.navDinamic )
-
-
+    
 module.exports = () => router;
