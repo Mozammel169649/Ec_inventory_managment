f457d354981107b354ed5003fc6eca4e04336b32
mozammel <mozammel169649@gmail.com>
2024-04-04 17:53:12 +0600

    store

--- react_app/author/admin/redux/app/store.js
+++ react_app/author/admin/redux/app/store.js
@@ -1,19 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "../features/user/userSlice";
-import dataSlice from "../features/user/dataSlice";
+// import dataSlice from "../features/user/dataSlice";
 import roleSlice from "../features/user/roleSlice";
 import categorySlice from "../features/category/categorySlice";
 import brandSlice from "../features/brand/brandSlice";
 import supplierSlice from "../features/supplier/supplierSlice";
+import productSlice from "../features/product/productSlice";
+import stockSlice from "../features/stock/stockSlice";
 
 const store = configureStore({
     reducer: {
       users : userSlice,
-      data : dataSlice,
+      // data : dataSlice,
+      product : productSlice,
       role : roleSlice,
       category : categorySlice,
       brand : brandSlice,
-      supplier : supplierSlice
+      supplier : supplierSlice,
+      stock : stockSlice,
     }
 })
 
