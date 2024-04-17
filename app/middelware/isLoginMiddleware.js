b15afbfa65a7095916272dc409af2cbf8e83262a
mozammel <mozammel169649@gmail.com>
2024-04-17 13:17:07 +0600

    middleware

---
+++ app/middelware/isLoginMiddleware.js
@@ -0,0 +1,27 @@
+
+var jwt = require('jsonwebtoken');
+const userModel = require('../models/product_models/user.model');
+
+
+const isLogin = async (req, res, next) => {
+    var { atoken } = req.cookies;
+   
+    if (atoken) {
+        try {
+            let auth_info = await jwt.verify(atoken, '6fd286f7-708a-429b-b53a-2bc5272e0db6');
+            const userRole = await userModel.findOne({ _id: auth_info._id });
+            if (userRole ) {
+                return next();
+            } else {
+                return res.redirect('/');
+            }
+        } catch (error) {
+            console.log("Error:", error);
+            return res.redirect('/#login');
+        }
+    } else {
+        return res.redirect('/#login');
+    }
+};
+
+module.exports = () => isLogin;
